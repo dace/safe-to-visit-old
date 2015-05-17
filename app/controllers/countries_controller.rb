00d71e4e0365eb0257e90871c8884e00c8404eef
hidace <dacewillmott@gmail.com>
2015-05-17 09:03:11 -0400

    add pry

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,7 +5,10 @@ class CountriesController < ApplicationController
     travel_alerts = Hash.from_xml(Net::HTTP.get_response(URI.parse('http://travel.state.gov/_res/rss/TAs.xml')).body).to_json
     @alerts = GovAlert.create(content: travel_alerts)
 
+    
+
     travel_warnings = Hash.from_xml(Net::HTTP.get_response(URI.parse('http://travel.state.gov/_res/rss/TWs.xml')).body).to_json
     @warnings = GovWarning.create(content: travel_warnings)
+
   end
 end
